ede21bca5517e4513de400f655c96a4c1f0c61a8
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-23 11:34:01 +0100

    Update Summing.py
    Added documentation
    Added three number addition
    Added test case

--- Summing.py
+++ Summing.py
@@ -4,9 +4,16 @@ Created on Thu May 23 11:07:19 2024
 
 @author: willom
 """
-
-def add(a,b):
-    ans = a+b
+# Function to add three numbers
+def add(a,b, c=0):
+    ans = a+b+c
     return ans
 
 print(add(3,4))
+
+# test case
+# 1+2+3=6
+if add(1,2,3) == 6:
+    print("Test passed")
+else:
+    print("Test failed")
