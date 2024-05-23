7afe86c1ec4ffb4b3915f52085af706e79a162be
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-23 12:11:16 +0100

    Update mult.py
    Added user input functionality

--- mult.py
+++ mult.py
@@ -4,11 +4,16 @@ Created on Thu May 23 11:14:28 2024
 
 @author: willom
 """
-
-def multi(c,d):
+# added user input 
+def multi(c=0,d=0, user = False):
+    if user:
+        c = int(input("c = "))
+        d = int(input("d = "))
     ans = c*d
     return ans
 
 print(multi(4,6))
+
+print(multi(user = True))
 
 
