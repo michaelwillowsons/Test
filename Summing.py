d1e529836ab9ddcd6e868873c0aac0be1b0576a8
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-23 11:38:59 +0100

    Update Summing.py
    Added three default values

--- Summing.py
+++ Summing.py
@@ -5,7 +5,8 @@ Created on Thu May 23 11:07:19 2024
 @author: willom
 """
 # Function to add three numbers
-def add(a,b, c=0):
+# Default values
+def add(a=1,b=2,c=3):
     ans = a+b+c
     return ans
 
